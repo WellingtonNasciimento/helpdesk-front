325ad9ec9b5f9a411de9e14e74943915db15c424
WellingtonNasciimento <wellington.d.nascimento@uol.com.br>
2023-04-07 14:15:02 -0300

    Interceptor for HTTP Requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { ToastrModule } from 'ngx-toastr';
+import { AuthInterceptorProvider } from './interceptors/authentication.interceptor';
 
 // Componentes do projeto
 //import { NavComponent } from './components/nav/nav.component';
@@ -113,7 +114,7 @@ import { ToastrModule } from 'ngx-toastr';
     })
 //    NgxMaskModule.forRoot()
   ],
-  providers: [],
+  providers: [AuthInterceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
